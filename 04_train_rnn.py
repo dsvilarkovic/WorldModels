50f2f01eaf9f21fe951d4a562b4dbe602082f9bd
Dusan Svilarkovic <dsvilarko@lo-login-01.leonhard.ethz.ch>
2019-12-31 01:05:52 +0100

    Changed to make it work

--- 04_train_rnn.py
+++ 04_train_rnn.py
@@ -1,4 +1,4 @@
-#python 04_train_rnn.py --new_model --batch_size 200
+# python 04_train_rnn.py --new_model --batch_size 200
 # python 04_train_rnn.py --new_model --batch_size 100
 
 from rnn.arch import RNN
@@ -8,29 +8,30 @@ import os
 
 ROOT_DIR_NAME = './data/'
 SERIES_DIR_NAME = './data/series/'
+Z_DIM = 32
 
 
 def get_filelist(N):
-    filelist = os.listdir(SERIES_DIR_NAME)
-    filelist = [x for x in filelist if x != '.DS_Store']
-    filelist.sort()
-    length_filelist = len(filelist)
+	filelist = os.listdir(SERIES_DIR_NAME)
+	filelist = [x for x in filelist if x != '.DS_Store']
+	filelist.sort()
+	length_filelist = len(filelist)
 
+	if length_filelist > N:
+	  filelist = filelist[:N]
 
-    if length_filelist > N:
-      filelist = filelist[:N]
+	if length_filelist < N:
+	  N = length_filelist
 
-    if length_filelist < N:
-      N = length_filelist
-
-    return filelist, N
+	return filelist, N
 
 
 def random_batch(filelist, batch_size):
 	N_data = len(filelist)
 	indices = np.random.permutation(N_data)[0:batch_size]
 
-	z_list = []
+	z_listS = []
+	z_listB = []
 	action_list = []
 	rew_list = []
 	done_list = []
@@ -39,42 +40,53 @@ def random_batch(filelist, batch_size):
 		try:
 			new_data = np.load(SERIES_DIR_NAME + filelist[i])
 
-			mu = new_data['mu']
-			log_var = new_data['log_var']
+			muS = new_data['muS']
+			log_varS = new_data['log_varS']
+			muB = new_data['muB']
+			log_varB = new_data['log_varB']
 			action = new_data['action']
 			reward = new_data['reward']
 			done = new_data['done']
 
-			reward = np.expand_dims(reward, axis=2)
-			done = np.expand_dims(done, axis=2)
 
 
-			s = log_var.shape
+			reward = np.expand_dims(reward, axis=1)
+			# reward = np.expand_dims(reward, axis=2)
+			
+			done = np.expand_dims(done, axis=1)
+			# done = np.expand_dims(done, axis=2)
+			print(reward.shape)
+			s = log_varS.shape
 
-			z = mu + np.exp(log_var/2.0) * np.random.randn(*s)
+			zS = muS + np.exp(log_varS/2.0) * np.random.randn(*s)
+			zB = muB + np.exp(log_varB/2.0) * np.random.randn(*s)
 
-			z_list.append(z)
+			z_listS.append(zS)
+			z_listB.append(zB)
 			action_list.append(action)
 			rew_list.append(reward)
 			done_list.append(done)
 		except:
 			pass
 
-	z_list = np.array(z_list)
-	action_list = np.array(action_list)
+	z_listS = np.array(z_listS)
+	z_listB = np.array(z_listB)
 	rew_list = np.array(rew_list)
 	done_list = np.array(done_list)
+	action_list = np.array(action_list)
 
-	return z_list, action_list, rew_list, done_list
+	print(z_listB.shape, z_listS.shape, rew_list.shape, done_list.shape)
+	return z_listS, z_listB, action_list, rew_list, done_list
+
 
 def main(args):
-	
+
 	new_model = args.new_model
 	N = int(args.N)
 	steps = int(args.steps)
 	batch_size = int(args.batch_size)
 
-	rnn = RNN() #learning_rate = LEARNING_RATE
+	rnn = RNN()  # learning_rate = LEARNING_RATE
 
 	if not new_model:
 		try:
@@ -83,25 +95,26 @@ def main(args):
 			print("Either set --new_model or ensure ./rnn/weights.h5 exists")
 			raise
 
-
 	filelist, N = get_filelist(N)
 
-
 	for step in range(steps):
-		print('STEP ' + str(step))
+		print('STEP ' + str(step)+'/'+str(steps))
+		zS, zB, action, rew ,done = random_batch(filelist, batch_size)
 
-		z, action, rew ,done = random_batch(filelist, batch_size)
-
-		rnn_input = np.concatenate([z[:, :-1, :], action[:, :-1, :], rew[:, :-1, :]], axis = 2)
-		rnn_output = np.concatenate([z[:, 1:, :], rew[:, 1:, :]], axis = 2) #, done[:, 1:, :]
-
+		# print(zS[:, :-1, :].reshape(batch_size, -1, Z_DIM).shape)
+		# print(action[:, :-1, :].shape)
+		# print(rew[:, :-1, :].shape)
+		rnn_input = np.concatenate([zS[:, :-1, :].reshape(batch_size, -1, Z_DIM), action[:, :-1, :], rew[:, :-1, :]], axis = 2)
+		# print(rnn_input.shape)
+		rnn_output = np.concatenate([zB[:, 1:, :].reshape(batch_size, -1, Z_DIM), rew[:, 1:, :]], axis = 2) #, done[:, 1:, :]
+    
 		if step == 0:
 			np.savez_compressed(ROOT_DIR_NAME + 'rnn_files.npz', rnn_input = rnn_input, rnn_output = rnn_output)
-
+    
 		rnn.train(rnn_input, rnn_output)
-
-		if step % 10 == 0:
-
+    
+		if step % 10 == 0:    
+			print(step)
 			rnn.model.save_weights('./rnn/weights.h5')
 
 	rnn.model.save_weights('./rnn/weights.h5')
--- README.md
+++ README.md
@@ -17,3 +17,12 @@ https://github.com/gdrive-org/gdrive
 
 We also need some Google Drive API adjustments so follow instructions on this issue:
 https://github.com/gdrive-org/gdrive/issues/506#issuecomment-567253689
+
+For getting needed packages:
+module load StdEnv openmpi/4.0.1 openblas/0.2.19 libpng/1.6.27 gtkplus/3.20.10 mesa/12.0.3 sdl2/2.0.5 gcc/4.8.5 cmake/3.4.3 jpeg/9b python_gpu/3.6.1 mesa-glu/9.0.0 libgme/0.6.2 boost/1.62.0 eth_proxy mesa/12.0.6 mesa-glu/9.0.0 opencv/3.4.6 ffmpeg/3.2.14 hdf5/1.10.1
+
+python -m pip install --user keras
+python -m pip 
+For running RNN train:
+
+<code>python 04_train_rnn.py --new_model --batch_size 100</code>
